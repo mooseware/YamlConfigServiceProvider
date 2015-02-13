5ab9af40424b95cbf9d4daa50a65ccef95d58c3e
Bastian Martin <bastian@mooseware.org>
2015-02-13 13:03:15 +0100

    modifications
    Signed-off-by: Bastian Martin <bastian@mooseware.org>

--- src/Mooseware/Silex/YamlConfigServiceProvider.php
+++ src/Mooseware/Silex/YamlConfigServiceProvider.php
@@ -8,5 +8,26 @@ use Symfony\Component\Yaml\Yaml;
 
 class YamlConfigServiceProvider implements ServiceProviderInterface
 {
+    protected $environment = "dev";
+    protected $configPath = "";
+
+    public function __construct(Array $params) {
+        if ( isset($params['envVarName']) and ! empty($_SERVER[$params['envVarName']]))
+            $this->environment = $_SERVER[$params['envVarName']];
+
+        if (isset($params['configPath']))
+            $this->configPath = realpath($params['configPath']);
+    }
+
+    public function register(Application $app) {
+        die(var_dump($this->environment));
+        die(var_dump($this->configPath));
+        // @todo: find file based on app configuration
+        // @todo: read file
+
+    }
+
+    public function boot (Application $app) {
+    }
 
 }
