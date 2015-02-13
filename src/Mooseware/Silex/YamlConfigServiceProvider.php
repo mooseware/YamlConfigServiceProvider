6afcf66aa45e3bd22d6e7dc9c2e28d7115c2fdf1
Bastian Martin <bastian@mooseware.org>
2015-02-13 15:32:50 +0100

    further work
    Signed-off-by: Bastian Martin <bastian@mooseware.org>

--- src/Mooseware/Silex/YamlConfigServiceProvider.php
+++ src/Mooseware/Silex/YamlConfigServiceProvider.php
@@ -8,8 +8,10 @@ use Symfony\Component\Yaml\Yaml;
 
 class YamlConfigServiceProvider implements ServiceProviderInterface
 {
-    protected $environment = "dev";
-    protected $configPath = "";
+    protected
+        $environment = "dev",
+        $configPath = "",
+        $configFile = "";
 
     public function __construct(Array $params) {
         if ( isset($params['envVarName']) and ! empty($_SERVER[$params['envVarName']]))
@@ -17,17 +19,30 @@ class YamlConfigServiceProvider implements ServiceProviderInterface
 
         if (isset($params['configPath']))
             $this->configPath = realpath($params['configPath']);
+
+        $this->configFile = $this->configPath . DIRECTORY_SEPARATOR . "config." . $this->environment . ".yaml";
     }
 
     public function register(Application $app) {
-        die(var_dump($this->environment));
-        die(var_dump($this->configPath));
-        // @todo: find file based on app configuration
-        // @todo: read file
+        $config = $this->_parseConfigFile();
 
+        if (is_array($config)) {
+            if (isset($app['config']) and is_array($app['config'])) {
+                $app['config'] = array_replace_recursive($app['config'], $config);
+            } else {
+                $app['config'] = $config;
+            }
+        }
     }
 
-    public function boot (Application $app) {
+    public function boot(Application $app) {
+    }
+
+    private function _parseConfigFile() {
+        if ( ! file_exists($this->configFile))
+            throw new \InvalidArgumentException(sprintf("Config file '%s' was not found in '%'!", $this->configFile, $this->configPath));
+
+        return Yaml::parse($this->configFile);
     }
 
 }
