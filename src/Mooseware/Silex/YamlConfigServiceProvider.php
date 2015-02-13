d96544c7fbdbfdc6ecd80a4d968d2bd2f61f6d92
Bastian Martin <bastian@mooseware.org>
2015-02-13 12:07:17 +0100

    initial commit
    Signed-off-by: Bastian Martin <bastian@mooseware.org>

---
+++ src/Mooseware/Silex/YamlConfigServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mooseware\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class YamlConfigServiceProvider implements ServiceProviderInterface
+{
+
+}
